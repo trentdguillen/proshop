76a307928993c3708acab48e73de27a9243ee499
Trent D Guillen <tdguillen1@gmail.com>
2024-02-05 12:01:42 -0500

    Added authSlice, FormContainer, LoginScreen, RegisterScreen,usersApiSlice, generateToken, authMiddleware,userController

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 
@@ -15,6 +17,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 };
