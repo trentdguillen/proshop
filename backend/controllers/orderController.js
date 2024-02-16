cc8ce56fc66e36a933408818467d69b11e9202c2
Trent D Guillen <tdguillen1@gmail.com>
2024-02-16 18:39:51 -0500

    added ProfileScreen, OrderScreen, and modified server.js, orderController.js, constants.js, index.js, ordersApiSlice, and usersApiSlice. NOTE: There is a bug in OrderScreen and it does not work

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -51,26 +51,49 @@ const getMyOrders = asyncHandler(async ( req, res) => {
 //desc      Get order by ID
 //route     GET /api/orders/:id
 //access    Private
-const getOrderById = asyncHandler(async ( req, res) => {
-    const order = await Order.findById(req.params.id).populate('user', 'name email');
-    
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'name email'
+    );
+  
     if (order) {
-        res.status(200).json(order);
+      res.json(order);
+    } else {
+      res.status(404);
+      throw new Error('Order not found');
+    }
+  });
+
+//desc      Update order to paid
+//route     PUT /api/orders/:id/pay
+//access    Private
+const updateOrderToPaid = asyncHandler(async ( req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.email_address,
+        }
+
+        //the above information will come from PayPal 
+
+        const updatedOrder = await order.save();
+
+        res.status(200).json(updatedOrder);
     } else {
         res.status(404);
         throw new Error('Order not found');
     }
 });
 
-//desc      Update order to paid
-//route     PUT /api/orders/:id/pay
-//access    Private
-const updateOrderToPaid = asyncHandler(async ( req, res) => {
-    res.send('update order to paid');
-});
-
 //desc      Update order to delivered
-//route     GET /api/orders/:id/deliver
+//route     PUT /api/orders/:id/deliver
 //access    Private/Admin
 const updateOrderToDelivered = asyncHandler(async ( req, res) => {
     res.send('update order to delivered');
