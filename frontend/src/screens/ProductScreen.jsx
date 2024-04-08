dcbe394dd84df60fece4179403cde4063d858023
Nick Jasper <Ncj20a@my.fsu.edu>
2024-04-07 21:00:14 -0400

    removed import that was called multiple times

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useNavigate, useParams, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   Row,
