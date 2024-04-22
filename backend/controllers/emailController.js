83ea45e377ef934d0b65ddb05acb5893c37190e3
Nick Jasper <Ncj20a@my.fsu.edu>
2024-04-22 17:07:41 -0400

    added .env variable for security

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -9,20 +9,20 @@ const sendEmail = asyncHandler(async (req, res) => {
 
   // Create a transporter using SMTP
   let transporter = nodemailer.createTransport({
-    host: 'smtp.elasticemail.com',
+    host: process.env.ELASTIC_EMAIL_HOST,
     port: 2525,
     secure: false,
     auth: {
-      user: 'nicko20971@gmail.com',
-      pass: '1A8F356F336CD7584A486F3192F35AC49595',
+      user: process.env.ELATIC_EMAIL_USER,
+      pass: process.env.ELASTIC_EMAIL_PASS,
     }
   });
 
   try {
     // Send mail with defined transport object
     let info = await transporter.sendMail({
-      from: 'nicko20971@gmail.com', // Sender address (must be same as auth.user)
-      to: 'njasper0126@gmail.com', // List of receivers
+      from: process.env.FROM_EMAIL, // Sender address (must be same as auth.user)
+      to: process.env.TO_EMAIL, // List of receivers
       subject: subject,
       text: message
     });
