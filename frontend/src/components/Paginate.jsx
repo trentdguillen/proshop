54a9bc0091ed7a8cd41c6e5cef2e0188a4508eac
Luncon <124539996+Luncon@users.noreply.github.com>
2024-04-28 13:37:11 -0400

    Fixed Paginate

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,58 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types'; // Import PropTypes
 import { Pagination } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const Paginate = ({ pages, page, isAdmin, keyword }) => {
 
-  let link;
-  if (!isAdmin) {
-    if (keyword) {
-      link = `/search/${keyword}/page/${x + 1}`;
-    } else {
-      link = `/page/${x + 1}`;
-    }
-  } else {
-    link = `/admin/productlist/${x + 1}`;
-  }
-
-  return (
-    pages > 1 && (
-      <Pagination>
-        {[...Array(pages).keys()].map((x) => {
-          let link;
-          if (!isAdmin) {
-            if (keyword) {
-              link = `/search/${keyword}/page/${x + 1}`;
-            } else {
-              link = `/page/${x + 1}`;
-            }
-          } else {
-            link = `/admin/productlist/${x + 1}`;
-          }
-          return (
-            <LinkContainer key={x + 1} to={link}>
-              <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
-            </LinkContainer>
-          );
-        })}
-      </Pagination>
-    )
-  );
+const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
+ return (
+   pages > 1 && (
+     <Pagination>
+       {[...Array(pages).keys()].map((x) => (
+         <LinkContainer
+           key={x + 1}
+           to={
+             !isAdmin
+               ? keyword
+                 ? `/search/${keyword}/page/${x + 1}`
+                 : `/page/${x + 1}`
+               : `/admin/productlist/${x + 1}`
+           }
+         >
+           <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+         </LinkContainer>
+       ))}
+     </Pagination>
+   )
+ );
 };
 
-// Define propTypes for the component
-Paginate.propTypes = {
-  pages: PropTypes.number.isRequired,
-  page: PropTypes.number.isRequired,
-  isAdmin: PropTypes.bool,
-  keyword: PropTypes.string
-};
-
-// Set default values for optional props
-Paginate.defaultProps = {
-  isAdmin: false,
-  keyword: ''
-};
 
 export default Paginate;
